c8b152902c7359d2b2b11f288ebe7c6bc2cae79d
afapdx <140339218+afapdx@users.noreply.github.com>
2023-09-19 17:38:36 +0200

    Update site

--- curs-2023-24/_module.js
+++ curs-2023-24/_module.js
@@ -4982,6 +4982,12 @@ function create_fragment$7(ctx) {
 					},
 					{
 						"link": {
+							"url": "/notcies-i-activitats",
+							"label": "Notícies i activitats"
+						}
+					},
+					{
+						"link": {
 							"url": "/curs-2023-24",
 							"label": "Curs 2023-24"
 						}
